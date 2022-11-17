171988ea8bc9a10541e136e13d453e754b4c2a6b
aga2cr5 <kodak2cr5@gmail.com>
2022-11-17 18:00:07 +0200

    muutettu install.sh käyttäjän haku docker runiin

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 mkdir memory
 docker build -t entropy-bot . --network host
-docker run --restart unless-stopped --name entropy-bot -v "$(pwd)":/memory --user 1000:1000 entropy-bot
+docker run --restart unless-stopped --name entropy-bot -v "$(pwd)":/memory --user $(id -u):$(id -g) entropy-bot
